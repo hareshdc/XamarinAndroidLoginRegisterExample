c9a4f2c6f205badb5d19ed4f3cbf8e51bf4d9a05
DESKTOP-6ICSE8H\cddat <hareshdc@gmail.com>
2018-08-05 11:33:36 -0700

    proj files

--- XALR/XALR/Resources/Resource.Designer.cs
+++ XALR/XALR/Resources/Resource.Designer.cs
@@ -41,11 +41,63 @@ namespace XALR
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f04000b
+			public const int btnBack = 2130968587;
+			
+			// aapt resource value: 0x7f04000a
+			public const int btnCreate = 2130968586;
+			
+			// aapt resource value: 0x7f040002
+			public const int btnSign = 2130968578;
+			
+			// aapt resource value: 0x7f040004
+			public const int btnSignUp = 2130968580;
+			
+			// aapt resource value: 0x7f040007
+			public const int edtEmail = 2130968583;
+			
+			// aapt resource value: 0x7f040008
+			public const int edtMobile = 2130968584;
+			
+			// aapt resource value: 0x7f040005
+			public const int edtfullname = 2130968581;
+			
+			// aapt resource value: 0x7f040009
+			public const int edtpassword = 2130968585;
+			
+			// aapt resource value: 0x7f040006
+			public const int edtusername = 2130968582;
+			
+			// aapt resource value: 0x7f040003
+			public const int txtOr = 2130968579;
+			
+			// aapt resource value: 0x7f040001
+			public const int txtpassword = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int txtusername = 2130968576;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f020000
 			public const int Main = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int RegistrationPage = 2130837505;
 			
 			static Layout()
 			{
